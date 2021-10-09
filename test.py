1f7e9e0a87cc441848623156f189a398ea098c50
Ricardo Gomes <truericardogomes@gmail.com>
2021-10-09 00:43:23 -0400

    Teste de Correção de  Falha de Pipeline na DIV Cabeçalho da Index.html

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - Ricardo Gomes")
+        self.assertRegex(result.data.decode(), "Pipeline (teste falha) DevOps - Ricardo Gomes")
 
 
 if __name__ == "__main__":
