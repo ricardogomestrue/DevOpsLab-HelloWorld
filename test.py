f42dded1824b6a6b148d7820cad4a920522c6774
ricardogomestrue <64811124+ricardogomestrue@users.noreply.github.com>
2021-10-10 21:44:00 -0300

    Update test.py

--- test.py
+++ test.py
@@ -20,6 +20,38 @@ class Test(unittest.TestCase):
 
         # verifica o retorno do conteudo da pagina
         self.assertRegex(result.data.decode(), "Pipeline DevOps - Ricardo Gomes")
+       
+ 
+
+if __name__ == "__main__":
+    print ('INICIANDO OS TESTES')
+    print('----------------------------------------------------------------------')
+    unittest.main(verbosity=2)
+    
+    
+    
+from app import app
+import unittest
+
+class Test(unittest.TestCase):
+    
+    def setUp(self):
+        # cria uma instancia do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+
+    def test_requisicao(self):
+        # envia uma requisicao GET para a URL
+        result = self.app.get('/')
+
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(result.status_code, 200) 
+
+    def test_conteudo(self):
+        # envia uma requisicao GET para a URL
+        result = self.app.get('/') 
+
+        # verifica o retorno do conteudo da pagina
+       
         self.assertRegex(result.data.decode(), "Front-End via GitHub- Ricardo Gomes")
  
 
